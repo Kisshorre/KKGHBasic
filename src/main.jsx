3532ecb58c56df3c6229ffcbbd44c83e820bf3d5
Kishore <kisshorre100@gmail.com>
2026-04-02 12:42:48 +0100

    App is imported

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 
 import ReactDOM from 'react-dom/client';
-
+import React from 'react';       // required by ESLint
+import { createRoot } from 'react-dom/client';
+import App from './App';         // define App
 
 import './index.css';
 
